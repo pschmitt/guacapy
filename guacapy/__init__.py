d15588d260dd034c1427b09eae0f5cbe623f2094
Jordan Facibene <jordan@networktocode.com>
2018-07-05 11:03:51 -0400

    add a python3 compatible import

--- guacapy/__init__.py
+++ guacapy/__init__.py
@@ -1,2 +1,7 @@
-from client import Guacamole
-from templates import *
+import sys
+if sys.version_info[0] < 3:
+    from client import Guacamole
+    from templates import *
+else:
+    from .client import Guacamole
+    from .templates import *
