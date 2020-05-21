cd0323e172af887c2a8465e1c94a7d3df6b1673d
Philipp Schmitt <philipp@schmitt.co>
2020-05-21 21:06:44 +0200

    Add license

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ from setuptools import find_packages, setup
 setup(
     name="guacapy",
     version="0.9.6",
+    license="GPL3",
     description="REST API client for Guacamole",
     author="Philipp Schmitt",
     author_email="philipp@schmitt.co",
