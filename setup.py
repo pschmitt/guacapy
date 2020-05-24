79885304108701d9775ce6e8f5fb576b5273b196
Philipp Schmitt <philipp@schmitt.co>
2020-05-24 09:32:50 +0200

    Version 0.9.7

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="guacapy",
-    version="0.9.6.post1",
+    version="0.9.7",
     license="GPL3",
     description="REST API client for Guacamole",
     author="Philipp Schmitt",
